7f3c7cc401e139936339660ffadd31c918277fdc
Ryan VanDenburgh <vandrj63@oneonta.edu>
2014-02-28 15:49:41 -0500

    Simple form

---
+++ employee/employeeForm.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Employee name: <?php echo $_GET["employeeName"]; ?><br>
+Employee Number: <?php echo $_GET["employeeNumber"]; ?><br>
+phone number: <?php echo $_GET["phoneNumber"]; ?><br>
+address: <?php echo $_GET["address"]; ?><br>
+city: <?php echo $_GET["city"]; ?><br>
+state: <?php echo $_GET["states"]; ?><br>
+zip: <?php echo $_GET["zip"]; ?><br>
+current employee: <?php echo $_GET["currentEmployee"]; ?>
+
+</body>
+</html>
